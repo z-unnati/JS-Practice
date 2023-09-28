be7c2f11665493b4c66c2714588f8d3cd6d838d5
z-unnati <unnati.chhabra@zomato.com>
2023-09-28 11:30:42 +0530

    practice

---
+++ practice/script.js
@@ -0,0 +1,4 @@
+console.log("hello, i am unnati!")
+var a = 6;
+let b = 9;
+console.log(a, b)
---
+++ practice/variables.js
@@ -0,0 +1,10 @@
+let a = 9;
+a = "unnati";
+
+
+var b = "Zomato"; // old js use var 
+
+
+console.log(a, a, b);
+
+
